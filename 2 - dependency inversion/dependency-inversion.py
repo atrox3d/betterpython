efb139ea0fb46318c87f542f821767b3c89ae796
atrox3d <atrox3d@gmail.com>
2023-12-05 11:35:05 +0100

    add Switchable abstract class

--- 2 - dependency inversion/dependency-inversion.py
+++ 2 - dependency inversion/dependency-inversion.py
@@ -1,3 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Switchable(ABC):
+    @abstractmethod
+    def turn_on(self):
+        pass
+
+    @abstractmethod
+    def turn_off(self):
+        pass
+
+
 class LightBulb:
     def turn_on(self):
         print("LightBulb: turned on...")
