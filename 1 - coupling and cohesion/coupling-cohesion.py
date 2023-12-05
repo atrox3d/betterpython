04dd1f4b54ac9550ac1719e993c29e7870d6ce93
atrox3d <atrox3d@gmail.com>
2023-12-05 08:24:01 +0100

    move registry methods to VehicleRegistry

--- 1 - coupling and cohesion/coupling-cohesion.py
+++ 1 - coupling and cohesion/coupling-cohesion.py
@@ -17,31 +17,48 @@ class Vehicle:
     info: VehicleInfo
 
 
-vi = VehicleInfo('brand', 10000, True)
-v = Vehicle('id', '123456', vi)
+# vi = VehicleInfo('brand', 10000, True)
+# v = Vehicle('id', '123456', vi)
 
 
 class VehicleRegistry:
+    vehicle_info = {}
+
+    def add_vehicle_info(self, brand, electric, catalogue_price):
+        self.vehicle_info[brand] = VehicleInfo(brand, catalogue_price, electric)
+
+    def __init__(self) -> None:
+        self.add_vehicle_info("Tesla Model 3", True, 60000)
+        self.add_vehicle_info("Volkswagen ID3", True, 35000)
+        self.add_vehicle_info("BMW 5", False, 45000)
 
     def generate_vehicle_id(self, length):
         return ''.join(random.choices(string.ascii_uppercase, k=length))
 
     def generate_vehicle_license(self, id):
         return f"{id[:2]}-{''.join(random.choices(string.digits, k=2))}-{''.join(random.choices(string.ascii_uppercase, k=2))}"
+    
+    def create_vehicle(self, brand):
+        # generate a vehicle id of length 12
+        vehicle_id = self.generate_vehicle_id(12)
+
+        # now generate a license plate for the vehicle
+        # using the first two characters of the vehicle id
+        license_plate = self.generate_vehicle_license(vehicle_id)
+
+        return Vehicle(vehicle_id, license_plate, self.vehicle_info[brand])
 
 
 class Application:
+
+    def __init__(self) -> None:
+        pass
 
     def register_vehicle(self, brand: string):
         # create a registry instance
         registry = VehicleRegistry()
 
-        # generate a vehicle id of length 12
-        vehicle_id = registry.generate_vehicle_id(12)
-
-        # now generate a license plate for the vehicle
-        # using the first two characters of the vehicle id
-        license_plate = registry.generate_vehicle_license(vehicle_id)
+        vehicle = registry.create_vehicle(brand)
 
         # compute the catalogue price
         catalogue_price = 0
