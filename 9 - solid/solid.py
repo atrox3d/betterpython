5bbad5822fcc2ee7f0ade659fd1e15c69244db54
atrox3d <atrox3d@gmail.com>
2023-12-11 06:00:32 +0100

    Liskov Substitution: move code/mail to init

--- 9 - solid/solid.py
+++ 9 - solid/solid.py
@@ -28,21 +28,32 @@ class PaymentProcessor(ABC):
 
 class DebitPaymentProcessor(PaymentProcessor):
 
-    def pay(self, order, security_code):
+    def __init__(self, security_code) -> None:
+        self.security_code = security_code
+
+    def pay(self, order):
         print("Processing debit payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
 
 class CreditPaymentProcessor(PaymentProcessor):
-    def pay(self, order, security_code):
+
+    def __init__(self, security_code) -> None:
+        self.security_code = security_code
+
+    def pay(self, order):
         print("Processing credit payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying security code: {self.security_code}")
         order.status = "paid"
 
 class PaypalPaymentProcessor(PaymentProcessor):
-    def pay(self, order, security_code):
+
+    def __init__(self, email) -> None:
+        self.email = email
+
+    def pay(self, order):
         print("Processing Paypal payment type")
-        print(f"Verifying security code: {security_code}")
+        print(f"Verifying email: {self.email}")
         order.status = "paid"
 
 
@@ -52,5 +63,5 @@ order.add_item("SSD", 1, 150)
 order.add_item("USB cable", 2, 5)
 
 print(order.total_price())
-processor = PaypalPaymentProcessor()
-processor.pay(order, "0372846")
+processor = PaypalPaymentProcessor('mail@server')
+processor.pay(order)
