c5b2821c4053238dbb1b2af4a3fa6d8698ff9b8d
atrox3d <atrox3d@gmail.com>
2024-01-02 14:13:35 +0100

    remove code from abstract methods

--- 6 - template method & bridge/trading.py
+++ 6 - template method & bridge/trading.py
@@ -32,18 +32,12 @@ class TraderBot(ABC):
 
     @abstractmethod
     def should_buy(self, prices: List[float]) -> bool:
-        if self.trading_strategy == "minmax":
-            return prices[-1] == min(prices)
-        else:
-            return prices[-1] < self.list_average(prices)
+        pass
 
     @abstractmethod
     def should_sell(self, prices: List[float]) -> bool:
-        if self.trading_strategy == "minmax":
-            return prices[-1] == max(prices)
-        else:
-            return prices[-1] > self.list_average(prices)
-
+        pass
+    
     def check_prices(self, coin: str):
         self.exchange.connect()
         prices = self.exchange.get_market_data(coin)
